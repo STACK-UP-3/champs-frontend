11f51c475ad325a48683657842bcdae74091c536
Irutingabo <devel605@gmail.com>
2020-05-12 15:20:37 +0200

    [Finishes #171648111] Setup basic React routes

---
+++ src/Components/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => (
+	<Fragment>
+		<h1>Barefoot Nomad</h1>
+		<nav className="header">
+			<ul className="header">
+				<li><Link to="/"> Home </Link></li>
+				<li><Link to="/signin">Sign In</Link></li>
+			</ul>
+		</nav>
+		<hr />
+	</Fragment>
+);
+
+export default NavBar;
